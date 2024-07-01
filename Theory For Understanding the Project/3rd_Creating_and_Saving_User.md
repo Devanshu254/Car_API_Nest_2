8880e939aeb02f2c178f007a0dd7620989234782
Devanshu254 <devanshujha8305@gmail.com>
2024-07-01 21:31:27 +0530

    We have writtern a code to created and saved a user by using dependency injection system

---
+++ Theory For Understanding the Project/3rd_Creating_and_Saving_User.md
@@ -0,0 +1,5 @@
+# Creating and saving user.
+> We have setup our repositories by using Typeorm and now we are going to Create and Save user data within our application.
+> We are going to create Service methods. Which are then used to access Users Repository. We are going to do that hookup using Dependency Injection.
+
+> First We have created users.services.ts file and then we have imported it within users.controller.ts.
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    // Theory of code is writtern below for better understanding.
+    constructor(@InjectRepository(User) private repo: Repository<User>) {}
+    // We are going to define a class create. We need to take these email and password and use them along with our repository to create and save a new user in our repository.
+    create(email: string, password: string) {
+        const user = this.repo.create({email, password});
+        return this.repo.save(user); 
+    }
+}
+// Private -> Just to abbriviate that property definition and assignment.
+// repo: Repository<User> -> Type annotation is Repository and generic type is user, means that repo is an instance of typeorm repository that deals with instances of user.
+// @InjectRepository(User) -> This part is an aid to dependency injection system this is what is going to tell the dependency injection system that we need the user repository.Dependency injection system uses this type annotation right here to figure out what instance it needs to inject in this class at run time. Unfortunately the dependency injection system does not play nicely with generics so this decorator is required simply because we have to use a generic type right here.
+// We are going to define a method inside this class called create.
+
+
+// Now let us go and hookup this service back to our controller we are going to use the service to create a new user, then we will test it with our thunder client. Once again for hookup we are going to use dependency injection.
